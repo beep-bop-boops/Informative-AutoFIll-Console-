0565b30d7dd9af1d488e0f526662ba34bfa75cae
huseyin1596498 <huseyinucur@yahoo.co.uk>
2019-03-17 18:05:58 +0000

    0.1

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <time.h>
+
+
+using namespace std;
+
+int main()
+{
+ 
+ string a;
+  cout << a;
+ cin >> a;
+ cout << a;
+
+system("pause");
+    return 0;
+}
