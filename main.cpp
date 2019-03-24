d5e1a14b3f2104648ac275ca6948656ef603813c
huseyin1596498 <huseyinucur@yahoo.co.uk>
2019-03-24 23:53:16 +0000

    0.9

--- main.cpp
+++ main.cpp
@@ -5,21 +5,18 @@
 #include <cstdlib>
 #include <cstring>
 #include <time.h>
-#define _WIN32_WINNT 0*0502
+#define _WIN32_WINNT 0 * 0502
 using namespace std;
 
 string TxtInput;
 bool cmdfound = false;
 string LibArray[10][10][10][10][10];
 int arrayIndexAmount = 5;
-int LocAmount = 5;
-int usLoc[5];
+int LocAmount = 10;
+int usLoc[10];
 bool usLocB[5];
 bool exitDir = false;
 int Loc[5];
-
-
-
 
 void printdir();
 void initializeDictionary();
@@ -27,23 +24,20 @@ void TxtInputin();
 void TxtInputout();
 void cmd2in();
 void cmd2out();
-void opendi();
+
 void loadanimin();
 void loadanimout();
 void Subidirectory();
 void randcolour();
 void remove_scrollbar();
 
-
 int main()
 {
-    for(int x=0; x<LocAmount ; x++)
+    for (int x = 0; x < LocAmount; x++)
     {
         usLoc[x] = 0;
-        cout << usLoc[x];
-        
     }
-    srand (time(NULL));
+    srand(time(NULL));
     initializeDictionary();
     cout << "------------------------------------" << endl;
     cout << "|               _        _______   |" << endl;
@@ -62,11 +56,9 @@ int main()
     cout << "|                 IAC              |" << endl;
     cout << "------------------------------------" << endl;
 
-
     // iac 3.40 = iac 3= Libarray size 40 = usloc size.  dd
     //finding location . can tpye loc3.4.5 at any time to go into locus[1]=3 usloc[2]=4...
     // cant find current location loc = 4.5.6.7
-
 
     /*
     HWND console = GetConsoleWindow();
@@ -74,7 +66,7 @@ int main()
     GetWindowRect(console, &r); //stores the console's current dimensions
     MoveWindow(console, r.left, r.top, 338, 900, TRUE); // 800 width, 100 height.left, rect.bottom-rect.top, rect.right-rect.left, TRUE);  
     */
-    
+
     printdir();
 
     system("pause");
@@ -84,315 +76,140 @@ int main()
 
 void printdir()
 {
-    cmdfound = false;   
+    cmdfound = false;
     exitDir = false;
 
     cout << "IAC:\\";
 
-      if(usLocB[1] == true)
-                    {
-                         cout << endl << "0 is trueee" << endl;
-                        usLocB[0]= true;
-                    }
-                    else
-                    {
-                        usLocB[0]= false;
-                    }
-                    
-
-                    if(usLoc[1] > 0)
-                    {
-                         
-                        usLoc[0]= 1;
-                    }
-                    else
-                    {
-                        usLoc[0]= 0;
-                    }
-                    
-
-cout << endl << "print 1st printdir" << endl;
-     for(int x=0; x<5 ; x++)
+    if (usLocB[1] == true)
     {
-        
+        usLocB[0] = true;
+    }
+    else
+    {
+        usLocB[0] = false;
+    }
+
+    if (usLoc[1] > 0)
+    {
+
+        usLoc[0] = 1;
+    }
+    else
+    {
+        usLoc[0] = 0;
+    }
+
+    /*
+    for (int x = 0; x < 5; x++)
+    {
+
         cout << "usLocB[" << x << "] = " << usLocB[x] << endl;
-        
     }
 
-
-    for(int x=0; x<5 ; x++)
+    for (int x = 0; x < 5; x++)
     {
-        
+
         cout << "usLoc[" << x << "] = " << usLoc[x] << endl;
-        
     }
+    */
 
-    
- 
-
-    for(int n = 0; n<LocAmount; n++) 
+    for (int n = 0; n < LocAmount; n++)
     {
-        if(usLoc[n] > 0)
+        if (usLoc[n] > 0)
         {
             usLocB[n] = true;
         }
-        else 
+        else
         {
             usLocB[n] = false;
         }
     }
 
-
-
-
-cout << endl << "print 2nd after printdir" << endl;
-
-    for(int x=0; x<5 ; x++)
+    /*
+    for (int x = 0; x < 10; x++)
     {
-        
+
         cout << "usLocB[" << x << "] = " << usLocB[x] << endl;
-        
     }
 
-    for(int x=0; x<5 ; x++)
+    for (int x = 0; x < 10; x++)
     {
-        
+
         cout << "usLoc[" << x << "] = " << usLoc[x] << endl;
-        
     }
 
-    
-   
-   if(usLocB[1] == true){ cout << LibArray[usLoc[1]][0][0][0][0] << "\\" ;}
-   if(usLocB[2] == true){ cout <<  LibArray[usLoc[1]][usLoc[2]][0][0][0] << "\\";}
-   if(usLocB[3] == true){ cout << LibArray[usLoc[1]][usLoc[2]][usLoc[3]][0][0] << "\\";}
-   if(usLocB[4] == true){ cout << LibArray[usLoc[1]][usLoc[2]][usLoc[3]][usLoc[4]][0] << "\\";}
-   if(usLocB[5] == true){ cout << LibArray[usLoc[1]][usLoc[2]][usLoc[3]][usLoc[4]][usLoc[5]] << "\\";}
+*/
+    if (usLocB[1] == true)
+    {
+        cout << LibArray[usLoc[1]][0][0][0][0] << "\\";
+    }
+    if (usLocB[2] == true)
+    {
+        cout << LibArray[usLoc[1]][usLoc[2]][0][0][0] << "\\";
+    }
+    if (usLocB[3] == true)
+    {
+        cout << LibArray[usLoc[1]][usLoc[2]][usLoc[3]][0][0] << "\\";
+    }
+    if (usLocB[4] == true)
+    {
+        cout << LibArray[usLoc[1]][usLoc[2]][usLoc[3]][usLoc[4]][0] << "\\";
+    }
+    if (usLocB[5] == true)
+    {
+        cout << LibArray[usLoc[1]][usLoc[2]][usLoc[3]][usLoc[4]][usLoc[5]] << "\\";
+    }
 
-
-   
-
-    
-    
-   TxtInputin();
-
-
-} 
+    TxtInputin();
+}
 void TxtInputin()
 {
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     cmdfound = false;
-    
-
 
     getline(cin, TxtInput);
 
-
-//cout << endl << exitDir << endl;
-//cout << TxtInput << endl;
-
-
-
- if (TxtInput == "exit")
+    if (TxtInput == "exit")
     {
-       
-        
-        for(int a= 1; a<LocAmount; a++) 
-    {
-        
-        if(exitDir == false)
-        {
-       // cout << endl << "a = " << a << endl;
-        //cout << "locamount  =" << LocAmount ;
-        
-       // cout << "exit is fuck";
-        if(usLoc[a] > 0)
 
-        //cout << "usloc = " << usLoc[a] << endl;
+        for (int a = 1; a < LocAmount; a++)
         {
 
-           // cout << "if main accepted" << endl;
-
-            int d;
-            d = a + 1;
-
-            //cout << "usloc d =" << usLoc[d] << endl;
-
-           // cout << "d = " << d;  
-            if(usLoc[d] == 0)
+            if (exitDir == false)
             {
-                usLoc[a] = 0;
-               // cout << "loc =" << a << "has been butt fucked";
-                //cout << usLoc[1];
 
-                exitDir = true;
-              
-                
+                if (usLoc[a] > 0)
+
+                {
+
+                    loadanimin();
+                    SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_INTENSITY);
+                    cout << LibArray[usLoc[1]][usLoc[2]][usLoc[3]][usLoc[4]][usLoc[5]] << " directory left";
+                    SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_INTENSITY);
+                    loadanimout();
+                    SetConsoleTextAttribute(h, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN);
+
+                    int d;
+                    d = a + 1;
+
+                    if (usLoc[d] == 0)
+                    {
+                        usLoc[a] = 0;
+
+                        exitDir = true;
+                    }
+                }
             }
-          
-            
-        }
         }
 
-       
-    }
-   
-    printdir();
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-else {
-    for (int p=1;p<5;p++)
-    {
-        
-        
-     //   cout << "its has been run" << endl;
-
-
-
-
-        cout << "searching [" << p << "]" << endl;
-        if (usLocB[p] == false)
-         {
-             
-             cout << endl << "searching for being" << endl;
-             int r;
-             //cout <<  LibArray[usLoc[1]][usLoc[2]][usLoc[3]][usLoc[4]][usLoc[5]];   
-                                
-              for (r = 1; r<arrayIndexAmount; r++)
-             {
-                 //fucked used to be Usloc[p] = r
-                 
-                  //
-                    usLoc[p] = r;
-                    cout << "r = " <<  LibArray[usLoc[1]][usLoc[2]][usLoc[3]][usLoc[4]][usLoc[5]] << endl;
-                    cout << "text = " << TxtInput <<  endl ;
-                
-                
-                 if(TxtInput == LibArray[usLoc[1]][usLoc[2]][usLoc[3]][usLoc[4]][usLoc[5]])
-                 {
-                    cout << "searched Libarray[" << usLoc[1] << "]" << "[" << usLoc[2] << "]" << "[" << usLoc[3] << "]" << "[" << usLoc[4] << "]" << "[" << usLoc[5] << "]" << "[" << usLoc[6] << "]" << "[" << usLoc[7] << "]" << "[" << usLoc[8] << "]" << "[" << usLoc[9] << "]" << "[" << usLoc[10] << "]";
-                    cout << "r = " << r; 
-                    usLoc[p] = r; 
-                    cout << "p = " << p; 
-                    cout << "r = " << r; 
-                    r = arrayIndexAmount + 1;
-                    usLocB[p] = true;
-                    cmdfound = true;       
-                   
-                  cout << "searched Libarray[" << usLoc[1] << "]" << "[" << usLoc[2] << "]" << "[" << usLoc[3] << "]" << "[" << usLoc[4] << "]" << "[" << usLoc[5] << "]" << "[" << usLoc[6] << "]" << "[" << usLoc[7] << "]" << "[" << usLoc[8] << "]" << "[" << usLoc[9] << "]" << "[" << usLoc[10] << "]";
-                       
-                 }
-                 cout << "searched Libarray[" << usLoc[1] << "]" << "[" << usLoc[2] << "]" << "[" << usLoc[3] << "]" << "[" << usLoc[4] << "]" << "[" << usLoc[5] << "]" << "[" << usLoc[6] << "]" << "[" << usLoc[7] << "]" << "[" << usLoc[8] << "]" << "[" << usLoc[9] << "]" << "[" << usLoc[10] << "]";
-        
-                 
-              
-               
-                 
-                 
-        
-             }
-             
-        
-             
-                   if(cmdfound == false)
-                   {
-                        SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_INTENSITY);
-        cout << "Command Not Found" << endl;
-        SetConsoleTextAttribute(h, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-        cout << "setting [" << p << "]" << endl;
-         usLoc[p]=0;
-
-  printdir();
-                   }
-                   else
-                   {
-                       printdir();
-                   }
-                   
-       
-
-        
-        
-
-
-
-
-
-         }
-
-    }
-
-
-}
-    
-     
-    
-    if (cmdfound)
-    {
-
-        loadanimin();
-        cout << "Accesed to " << TxtInput << " directory granted";
-        loadanimout();
-
-        //opendi();
-
-    
-      
         printdir();
-
-    }
-     
-    
-    
-
-
-
-    
-
-    
-    
-/*
-        if (usLoc1 > 0 && usLoc2 == 0 && usLoc3 == 0)
-        {
-            usLoc1 = 0;
-            loadanimin();
-            cout << LibArray[usLoc1][usLoc2][usLoc3] << " directory left";
-            loadanimout();
-            input();
-        }
-
-       
-        else
-        {
-             SetConsoleTextAttribute(h, FOREGROUND_RED );
-            cout << "No directory to leave" << endl;
-            SetConsoleTextAttribute(h, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN);
-            input();
-        }
     }
 
     else if (TxtInput == "subd")
     {
         Subidirectory();
-        input();
+        printdir();
     }
 
     else if (TxtInput == "help")
@@ -423,89 +240,108 @@ else {
         cout << "|         severity of the bug.      |" << endl;
         cout << "------------------------------------" << endl;
 
-        input();
+        printdir();
     }
-    */
-    
-   
-   
-}
 
-void opendi()
-{
-    /*
- if(usLoc1 == 1)
-    {   
-         cout << "----------------------" << endl;
-        cout << "| Linux |  | Windows |  " << endl;
-        cout << "----------------------" << endl;
+    else
+    {
+        for (int p = 1; p < 5; p++)
+        {
+            if (usLocB[p] == false)
+            {
+                int r;
+                for (r = 1; r < arrayIndexAmount; r++)
+                {
 
+                    usLoc[p] = r;
+
+                    if (TxtInput == LibArray[usLoc[1]][usLoc[2]][usLoc[3]][usLoc[4]][usLoc[5]])
+                    {
+                        usLoc[p] = r;
+
+                        r = arrayIndexAmount + 1;
+
+                        usLocB[p] = true;
+
+                        cmdfound = true;
+                        loadanimin();
+                        cout << "Accesed to " << TxtInput << " directory granted";
+                        loadanimout();
+                    }
+                }
+
+                if (cmdfound == false)
+                {
+                    SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_INTENSITY);
+                    cout << "Command Not Found" << endl;
+                    SetConsoleTextAttribute(h, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+                    usLoc[p] = 0;
+
+                    printdir();
+                }
+                else
+                {
+                    printdir();
+                }
+            }
+        }
     }
-*/
 }
 
 void initializeDictionary()
 {
-//IAC:/
+    //IAC:/
     LibArray[0][0][0][0][0] = "";
     LibArray[1][0][0][0][0] = "windows.fo";
     LibArray[2][0][0][0][0] = "linux.fo";
     LibArray[3][0][0][0][0] = "macos.fo";
     LibArray[4][0][0][0][0] = "andriod.fo";
-//windows
+    //windows
 
     LibArray[1][1][0][0][0] = "terminal.fo";
     LibArray[1][2][0][0][0] = "applications.fo";
     LibArray[1][3][0][0][0] = "userstuff.fo";
 
-    LibArray[2][1][0][0][0] = "lterminal.fo";
+    LibArray[2][1][0][0][0] = "Network";
     LibArray[2][2][0][0][0] = "lapplications.fo";
     LibArray[2][3][0][0][0] = "luserstuff.fo";
 
 
-
-
+    LibArray[2][1][1][0][0] = "bruteForce.in";
 }
 
 void Subidirectory()
 {
-    /*
+
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (usLoc1 == 0)
-    {
-        SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-        cout << "IAC Subdirectorys found & listed :";
 
-        for (int p = 0; p < 4; p++)
+    for (int p = 1; p < arrayIndexAmount; p++)
+    {
+        if (usLocB[p] == false)
         {
+            SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+            cout << LibArray[usLoc[1]][usLoc[2]][usLoc[3]][usLoc[4]][usLoc[5]] << "Subdirectorys found & listed :";
+
+            int r;
+            for (r = 1; r < arrayIndexAmount; r++)
+            {
+
+                usLoc[p] = r;
+                SetConsoleTextAttribute(h, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN);
+                cout << "---";
+                randcolour();
+                Sleep(200);
+                cout << LibArray[usLoc[1]][usLoc[2]][usLoc[3]][usLoc[4]][usLoc[5]];
+            }
+
+            usLoc[p] = 0;
+            p = arrayIndexAmount + 1;
             SetConsoleTextAttribute(h, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN);
-            cout << "---";
-            randcolour();
-            Sleep(200);
-            cout <<  LibArray[p + 1][0][0];
+            cout << endl;
         }
-        cout << endl;
-        SetConsoleTextAttribute(h, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN);
     }
 
-    else if (usLoc2 == 0 && usLoc1 > 0)
-    {
-        SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-        cout << LibArray[usLoc1][0][0] << " Subdirectorys found & listed :" ;
-
-        for (int p = 0; p < 4; p++)
-        {
-
-            SetConsoleTextAttribute(h, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN);
-            cout << "---";
-            randcolour();
-            Sleep(200);
-            cout <<  LibArray[usLoc1][p+1][0];
-        }
-        cout << endl;
-        SetConsoleTextAttribute(h, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN);
-    }
-
+    /*
     else if (usLoc3 == 0 && usLoc1 > 0 && usLoc2 > 0)
     {
         SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
@@ -564,37 +400,82 @@ void randcolour()
 {
     int a;
 
-HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-a = rand() % 14 +1;
-if(a==0){SetConsoleTextAttribute(h, FOREGROUND_BLUE);}
-else if(a==1){SetConsoleTextAttribute(h, FOREGROUND_GREEN);}
-else if(a==2){SetConsoleTextAttribute(h, FOREGROUND_BLUE);}
-else if(a==3){SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_BLUE);}
-else if(a==4){SetConsoleTextAttribute(h, FOREGROUND_RED);}
-else if(a==5){SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_BLUE);}
-else if(a==6){SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN);}
-else if(a==7){SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);}
-else if(a==8){SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_BLUE);}
-else if(a==9){SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_GREEN);}
-else if(a==10){SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE);}
-else if(a==11){SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_RED);}
-else if(a==12){SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE);}
-else if(a==13){SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN);}
-else if(a==14){SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);}
-else {SetConsoleTextAttribute(h, FOREGROUND_GREEN);}
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    a = rand() % 14 + 1;
+    if (a == 0)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_BLUE);
+    }
+    else if (a == 1)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_GREEN);
+    }
+    else if (a == 2)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_BLUE);
+    }
+    else if (a == 3)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_BLUE);
+    }
+    else if (a == 4)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_RED);
+    }
+    else if (a == 5)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_BLUE);
+    }
+    else if (a == 6)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN);
+    }
+    else if (a == 7)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    }
+    else if (a == 8)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_BLUE);
+    }
+    else if (a == 9)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_GREEN);
+    }
+    else if (a == 10)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    }
+    else if (a == 11)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_RED);
+    }
+    else if (a == 12)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE);
+    }
+    else if (a == 13)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN);
+    }
+    else if (a == 14)
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    }
+    else
+    {
+        SetConsoleTextAttribute(h, FOREGROUND_GREEN);
+    }
 }
-
 
 void remove_scrollbar()
 {
     HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_SCREEN_BUFFER_INFO info;
     GetConsoleScreenBufferInfo(handle, &info);
-    COORD new_size = 
-    {
-        info.srWindow.Right - info.srWindow.Left + 1,
-        info.srWindow.Bottom - info.srWindow.Top + 1
-    };
+    COORD new_size =
+        {
+            info.srWindow.Right - info.srWindow.Left + 1,
+            info.srWindow.Bottom - info.srWindow.Top + 1};
     SetConsoleScreenBufferSize(handle, new_size);
 }
- 
